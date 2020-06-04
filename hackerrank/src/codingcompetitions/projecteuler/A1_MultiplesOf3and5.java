8b67770908fd5dabd9163a95838f5332df245f6c
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2020-06-04 18:56:49 +0300

    fix

--- hackerrank/src/codingcompetitions/projecteuler/A1_MultiplesOf3and5.java
+++ hackerrank/src/codingcompetitions/projecteuler/A1_MultiplesOf3and5.java
@@ -6,18 +6,17 @@
  * If we list all the natural numbers below 10 that are multiples of 3 or 5 , we get 3,5,6 and 9.
  * The sum of these multiples is is 23. Find the sum of all the multiples of 3 or 5 below N.
  * 
+ * First line contains T that denotes the number of test cases. This is followed by T lines, each containing an integer, N.
+ * 
  * 
  * --Turkish--
  * 10 a kadar olan , 3 veya 5 'in katlari olan sayilara bakarsak  3, 5, 6 ve 9 u buluruz. Toplamlarıda 23 olur.
  * Bizden  girilen N sayisi icin benzer sekilde 3 veya 5 in kati olan sayılarin toplamini bulmamizi istiyor.
  * 
- *  
- * input : 
+ * 
  * ilk girilen sayi test sayisi
  * daha sonra test sayisi kadar N girilicek
- * 
- * output
- * her test için toplamlari yazmamiz lazim
+ * her test için toplamlari yazmamiz lazim.
  * 
  */
 package codingcompetitions.projecteuler;
@@ -26,6 +25,7 @@ import java.util.Scanner;
 
 public class A1_MultiplesOf3and5 {
 	
+	// Dont do this :)
 /*    private static int multiplesOf3and5(int topNumber) {
         int sum = 0;
         for (int i = 3; i < topNumber; i++) {
@@ -38,55 +38,82 @@ public class A1_MultiplesOf3and5 {
         return sum;
     }*/
 
-	private static int sumOfMultiples3or5(double topNumber) {
-		/*
-		 * sum for numbers divided to 3 is (3 + 6 + 9 + N + ... + = ...
-		 * sum for numbers divided to 5 is (5 + 10 + 15 + N + ... + = ...
-		 * 
-		 * 
-		 * 
-		 * sum of consecutive numbers
-		 * ((Last Number - First Number) / Difference + 1) * ((Last Number + First Number) / 2 ) 
-		 * 
-		 */
-		double sumOfNumbersDividedBy3 = 0;
-		double sumOfNumbersDividedBy5 = 0;
-		double sumOfNumbersDividedBy15 = 0;
-		for (double i = topNumber - 1; i > 2; i--) {
-			if(i % 3 == 0){
-				sumOfNumbersDividedBy3 = (((i - 3) / 3) + 1) * ((i  + 3) / 2);
-				break;
-			}
-		}
-		for (double i = topNumber - 1; i > 4; i--) {
-			if(i % 5 == 0){
-				sumOfNumbersDividedBy5 = (((i - 5) / 5) + 1) * ((i  + 5) / 2);
-				break;
-			}
-		}
-		for (double i = topNumber - 1; i > 14; i--) {
-			if(i % 15 == 0){
-				sumOfNumbersDividedBy15 = (((i - 15) / 15) + 1) * ((i  + 15) / 2);
-				break;
-			}
-		}
+	/*
+	 * 3 ve 5 'in katlarinin toplamini bulmak icin, matematikteki ardisik sayilarin toplami formulunu kullanacagiz.
+	 * [               Terim Sayisi                ] * [     Aritmetik Ortalama    ]
+	 * ((Son Terim - Ilk Terim) / Artis miktari + 1) * ( Son terim + Ilk Terim ) / 2
+	 * 
+	 * 15 ve katlari olan sayilar , hem 3 icin yaptigimiz hesaplamada, hem de 5 icin yaptigimiz hesaplamada toplama dahil olacagi icin 
+	 * bu sayilarin toplam degerini bulup hesaplamamizdan cikaracagiz ki ayni sayilari iki kere toplamis olmayalim.
+	 * 
+	 */
+	public static long sumOfMultiples3or5(long N) {
 		
-		
-		return (int)(sumOfNumbersDividedBy3 + sumOfNumbersDividedBy5 - sumOfNumbersDividedBy15);
+		// 3,5 ve 15 in katlarinin toplamlarini tuttugumuz degiskenler
+        long sumOfMultiples3 = 0;
+        long sumOfMultiples5 = 0;
+        long sumOfMultiples15 = 0;
+        
+        /*
+         * N olarak 10 girildiyse , 10 hesaplamaya dahil edilmesin isteniyor soruda , bu yuzden (N - 1)'den baslayarak , 1' e dogru giderken
+         * 3, 5 ve 15 i tam bolebilen en buyuk sayilari bulacagiz.
+         * 
+         * Ilk terimlerimiz ve artis miktarlarimiz 3, 5 ve 15 oluyor , en buyuk sayiyi bulduktan sonra toplam formulunu hesaplayacagiz.  
+         */
+        
+        for (long i = N - 1; i >= 1; i--) {
+            if(i % 3 == 0){
+                sumOfMultiples3 = sumOfConsecutiveNumbers(3, i, 3);
+                break;
+            }
+        }
+        for (long i = N - 1; i >= 1; i--) {
+            if(i % 5 == 0){
+                sumOfMultiples5 = sumOfConsecutiveNumbers(5, i, 5);
+                break;
+            }
+        }
+        for (long i = N - 1; i >= 1; i--) {
+            if(i % 15 == 0){
+                sumOfMultiples15 = sumOfConsecutiveNumbers(15, i, 15);
+                break;
+            }
+        }
+        return (sumOfMultiples3 + sumOfMultiples5 - sumOfMultiples15);
+    }
+	
+	/**
+	 * 
+	 * @param firstNumber : first number of consecutive numbers
+	 * @param lastNumber : last number of consecutive numbers
+	 * @param difference : difference between each number
+	 * @return firstNumber + (firstNumber + difference) + ( firstNumber + (2*difference)) + (firstNumber + (3*difference))... + lastNumber
+	 */
+	public static long sumOfConsecutiveNumbers(long firstNumber, long lastNumber , int difference){
+		return ((lastNumber - firstNumber) / difference + 1) * (lastNumber  + firstNumber) / 2;
 	}
+	
+	/*
+	 * ilk terimlerimiz sabit oldugu icin terim sayisini buldugumuz formulu son terim / artis miktari olarak, sadelestirebiliriz ve asagidaki gibi hesaplayabiliriz.
+	 * 
+	 * sumOfMultiples3 = sumOfConsecutiveNumbers(3, i, 3); yerine sumOfMultiples3 = (i / 3) * (i  + 3) / 2;
+	 * sumOfMultiples5 = sumOfConsecutiveNumbers(5, i, 5); yerine sumOfMultiples5 = (i / 5) * (i  + 5) / 2;
+	 * sumOfMultiples15 = sumOfConsecutiveNumbers(15, i, 15); yerine sumOfMultiples15 = (i / 15) * (i  + 15) / 2;
+	 * 
+	 */
+	
+	
 
 	public static void main(String[] args) {
-		
         Scanner in = new Scanner(System.in);
         System.out.print("Test case : ");
         int t = in.nextInt();
-        int[] arrayForN = new int[t];
         for(int i = 0; i < t; i++){
-        	System.out.print("N for " + (i + 1) + ". case : ");
-            arrayForN[i] = in.nextInt();
-            System.out.println(sumOfMultiples3or5(arrayForN[i]));
+        	System.out.print("N : ");
+            int N = in.nextInt();
+            System.out.println(sumOfMultiples3or5(N));
         }
-	}
+    }
 
 
 }
